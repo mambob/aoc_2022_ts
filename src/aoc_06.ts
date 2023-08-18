507f7bf739dd8204b779a263c58c8bcb36b22b73
Jorge Cabrera <jorge.cgc0@gmail.com>
2023-08-18 12:53:57 +0100

    guayandeishon

--- src/aoc_06.ts
+++ src/aoc_06.ts
@@ -1,4 +1,4 @@
-
+import { readFileSync } from "fs";
 
 // Este código se supone que funciona con los ejemplos de prueba, pendiente de hacer las pruebas
 export function datastreamMarkerFinder(buffer: string, markerLength: number = 4): number {
@@ -19,3 +19,16 @@ export function datastreamMarkerFinder(buffer: string, markerLength: number = 4)
     }
     return -1;
 }
+
+// Jorge del futuro, la versión optimizada de este algoritmo que teorizaste
+// no funciona en la práctica, porque no estás cacheando los resultados ni
+// nada parecido, así que existen casos que no se calculan.
+
+console.log(datastreamMarkerFinder(
+    readFileSync('./data/adventofcode.com_2022_day_6_input.txt', 'utf8')
+)); // = 1134
+
+console.log(datastreamMarkerFinder(
+    readFileSync('./data/adventofcode.com_2022_day_6_input.txt', 'utf8'),
+    14
+)); // = 2263
